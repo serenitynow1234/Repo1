89c0ece619409391de534197e2102a21fe1838ae
serenitynow1234 <robert.pace@outlook.com>
2025-10-08 09:24:17 +1100

    Delete extension directory

--- extension/background.js
+++
@@ -1,8 +0,0 @@
-// Initialize storage when the extension is installed.
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.get({prompts: []}, data => {
-    if (!Array.isArray(data.prompts)) {
-      chrome.storage.local.set({prompts: []});
-    }
-  });
-});
